4acc0d5b99c44e56d27ffab7c9ac7cd6c29da551
Nicole Speirs <nickispeirs@gmail.com>
2017-02-16 19:23:56 -0500

    release 1 -do the things

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+print "What is your name?"
+name = gets.chomp
+
+print "How old are you?"
+age = gets.chomp
+
+print "What year were you born?"
+year = gets.chomp
+
+print "Should we order some garlic for you?"
+garlic = gets.chomp
+
+print "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
+
