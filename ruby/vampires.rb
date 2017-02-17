c964008eb4f489ce036fcabe2e86c40bcf18c1c7
Nicole Speirs <nickispeirs@gmail.com>
2017-02-16 19:48:17 -0500

    saving work before GPS

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,6 +1,7 @@
 print "What is your name?"
 name = gets.chomp
 
+
 print "How old are you?"
 age = gets.chomp
 
@@ -12,4 +13,8 @@ garlic = gets.chomp
 
 print "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
+
+
+correct_age = 2017 - age == year
+puts correct_age
 
