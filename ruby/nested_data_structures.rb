729773dcec4de5a5af02383cfb3da76fab0c75ac
Nicole Speirs <nickispeirs@gmail.com>
2017-03-02 16:20:31 -0600

    added moving helper using array and hash

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,27 @@
+puts "Welcome to Moving Cross Country Helper!"
+move= {
+  "Rent new place"=>[
+    'zillow.com',
+    'rent.com',
+    'amli.com',
+    ],
+  "Get a moving truck"=>[
+    'bidget',
+    'uhaul',
+    'penske',
+    ],
+ "Change Address"=>[
+   'banks',
+   'electricity',
+   'internet',
+   'credit cards',
+    ],
+  "Pets"=>[
+    'vet visit',
+    'up to date on vaccinations',
+    'certificate of wellness',
+    'business card of old vet',
+    ]
+
+
+}
