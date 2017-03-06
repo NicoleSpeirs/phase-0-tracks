da3a52a6dd3b4bb3f3506dc02879018b7a67719a
Nicole Speirs <nickispeirs@gmail.com>
2017-03-06 15:44:08 -0600

    Completed 6.2 release 0

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -16,10 +16,47 @@
 # Licks
 # Bite
 
+# class Puppy
+# end
+
+# zoe = Puppy.new
+# toby = Puppy.new
+# berry = Puppy.new
+# toby.instance_of?(Puppy)
+
 class Puppy
+
+  def fetch(item)
+    puts "I brought back the #{item}!"
+    item
+  end
+
+  def speak(n=1)
+    puts "Woof! " * n
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    p human_years *7
+  end
+  def cookie_count(n=1)
+    puts "Puppy just ate #{n} Cookies!!"
+  end
 end
+
+
+
 
 zoe = Puppy.new
 toby = Puppy.new
-berry = Puppy.new
-toby.instance_of?(Puppy)
+
+zoe.fetch("ball")
+toby.fetch("green stinky toy")
+toby.speak(5)
+zoe.roll_over
+toby.dog_years(13)
+zoe.dog_years(4)
+toby.cookie_count(100)
