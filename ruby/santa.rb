38bd4c515823936344012477ba25315fee438f96
Nicole Speirs <nickispeirs@gmail.com>
2017-03-06 18:42:59 -0600

    Created a new file for 6.3 Attributes

---
+++ ruby/santa.rb
@@ -0,0 +1,16 @@
+class santa
+
+  def initiaize
+    puts "Initializing new santa instance"
+  end
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(type_of_cookie)
+    puts "That was a good #{type_of_cookie}"
+end
+
+
+
