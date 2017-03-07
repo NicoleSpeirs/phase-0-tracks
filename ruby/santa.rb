1518516e440614d071ffe430c41cb7f7de646a2e
Nicole Speirs <nickispeirs@gmail.com>
2017-03-06 20:10:23 -0600

    Updated santa class in 6.3 Release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,12 @@
 class Santa
-  def initialize
-    puts "Initializing Santa instance..."
+  def initialize(name, gender, ethnicity)
+    @name = name
+    @location = "The North Pole"
+    # puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -10,13 +16,33 @@ class Santa
   def eat_milk_and_cookies(type_of_cookie)
     puts "That was a good #{type_of_cookie} cookie!"
   end
+
+  def update_location(location)
+    @location = location
+  end
+
+  def about
+    puts "Name: #{@name}"
+    puts "Location: #{@location}"
+  end
 end
 
-bob = Santa.new
-jack = Santa.new
 
-bob.speak
-jack.eat_milk_and_cookies("monster")
+
+santa = Santa.new("Bob", "Male", "Asian")
+santa.update_location("New York")
+santa.about
+santa.speak
+santa.eat_milk_and_cookies("monster")
+# santa = Santa.new("Jack")
+
+# santa.speak
+# Jack.eat_milk_and_cookies("monster")
+santa = Santa.new("Jeff", "Male", "Italian")
+santa.update_location("Texas")
+santa.about
+santa.speak
+santa.eat_milk_and_cookies("yumsies")
 
 
 
