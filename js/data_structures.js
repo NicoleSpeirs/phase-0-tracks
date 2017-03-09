afacd78e8dad61955b9b02e0681a1190a071fd19
Nicole Speirs <nickispeirs@gmail.com>
2017-03-09 13:58:37 -0600

    added releae 1 do the thngs

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,5 +2,19 @@ var colors = ["blue", "pink", "purple", "red"];
 
 var horses = ["Sparkles", "Zoe", "Mini", "Violet"];
 
+console.log(horses[0]);
+
 colors.push("green");
 horses.push("Spot")
+
+console.log(horses);
+console.log(colors);
+
+for (var)
+
+
+
+var hourse_colors = {name: 'Sparkles', colors: 'blue'}
+var dog = {name: 'Spot', 'age': 3, isGoodDog: true};
+console.log("The dog's name is " + dog['name'] + ".");
+
