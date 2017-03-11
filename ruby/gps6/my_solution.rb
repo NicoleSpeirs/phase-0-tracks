6fee36cf000df78bdb569a7ec4592a56c2840466
Nicole Speirs <nickispeirs@gmail.com>
2017-03-11 11:43:13 -0600

    completed release 5,6,7,8

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,9 +4,10 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#require relative allows you to connect files that are in the same directory
+#require relative allows you to connect files that are in the same directory.
 #you can also use your current directory to require a path to other directories.
 require_relative 'state_data'
+
 
 class VirusPredictor
 
@@ -19,15 +20,15 @@ class VirusPredictor
 
 #it's a method that calls two methods
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    speed_of_spread
+    predicted_deaths
   end
 
   private
 
 #it's an instance method that accepts 3 arguments inside ( )
 #its calculates density ans predicts the number of deaths based on population
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -47,7 +48,7 @@ class VirusPredictor
 
 #it's an instance method that accepts 2 arguments
 #this starts with a speed of 0 and accelerates based on the population density
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -78,6 +79,9 @@ STATE_DATA.each do |state_name, value |
 
 state_virus_predictor = VirusPredictor.new(state_name,value[:population_density],value[:population])
 state_virus_predictor.virus_effects
+# state_virus_predictor.predicted_deaths
+# puts "\n"
+
 end
 
 
@@ -107,3 +111,16 @@ end
 
 #=======================================================================
 # Reflection Section
+
+#the different hush syntax in state_data.rb is THE hash rocket (=>) and the tailing symbol (:)
+#
+#require relative allows you to connect files that are in the same directory
+# in this case require_relative connects my_solution.rb file to state_data.rb
+# require is used to connect current directory to other directories but you
+# will need to specify where the file is for example: ../ or ./
+# we can use require './state_data' for this file and it will do the same as
+# require_relative.
+#
+#the concept that i solidified the most from this challenge is the difference
+# between require and require_relative as well as how to combine hashes using syntax
+# I also clarified the tern constant variable.
