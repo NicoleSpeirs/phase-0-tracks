fcb35f36cab0670575d9be098da49ed6298a2347
Nicole Speirs <nickispeirs@gmail.com>
2017-03-13 00:32:34 -0500

    completed release 1 in 7.3_algorithm

--- js/algos.js
+++ js/algos.js
@@ -22,3 +22,21 @@ function longestPhrase(arr) {
 
 console.log(longestPhrase(holidayNames));
 console.log(longestPhrase(fruits));
+
+function compare(obj1, obj2) {
+  for (var key in obj1) {
+    if (obj2[key] !== undefined) {
+      console.log("match!");
+      return true;
+    }
+  }
+  console.log("no matches found...");
+  return false;
+}
+
+var bob = {name: "Bob", age: 30, gender: "male"};
+var dylan = {name: "Dylan", age: 31};
+var toby = {legs: 4};
+
+console.log(compare(bob, dylan));
+console.log(compare(bob, toby));
