00a5e63ded42d38478183d57a4f1e5e4aca74c40
Nicole Speirs <nickispeirs@gmail.com>
2017-03-27 00:05:06 -0500

    add contact

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -23,6 +23,10 @@ get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
 
+get '/contact' do
+  "123 Learning Code Lane"
+end
+
 # write a GET route that retrieves
 # all student data
 get '/students' do
